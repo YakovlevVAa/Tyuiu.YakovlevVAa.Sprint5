b1f099042dbf2c507db26fdbc244ae10507f8993
Виктор Яковлев <Tyuiu.YakovlevVA@outlook.com>
2024-11-29 22:55:06 +0500

    .

--- Tyuiu.YakovlevVAa.Sprint5.Task1.V16.Lib/DataService.cs
+++ Tyuiu.YakovlevVAa.Sprint5.Task1.V16.Lib/DataService.cs
@@ -14,20 +14,22 @@ namespace Tyuiu.YakovlevVAa.Sprint5.Task1.V16.Lib
                 File.Delete(path);
             }
             double y;
-            string strY;
+            
             for (double x = startValue; x <= stopValue; x++)
             {
+                int numDigitsAfterPoint = 2;
+
                 y = Math.Sin(x) + ((2.0 * x) / 3.0) - (Math.Cos(x) * 4.0 * x);
                 y = Math.Round(y, 2);
-                strY = Convert.ToString(y);
+                string result = y.ToString("0."+ new string('0', numDigitsAfterPoint));
                 
                 if ( x != stopValue)
                 {
-                    File.AppendAllText(path, strY + Environment.NewLine);
+                    File.AppendAllText(path, result + Environment.NewLine);
                 }
                 else
                 {
-                    File.AppendAllText(path, strY);
+                    File.AppendAllText(path, result);
                 }
                 if (double.IsInfinity(y) || double.IsNaN(y))
                 {
